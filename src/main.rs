6d928a21d12551007e08e11d1a529422fdcb683d
Aleksey Kuznetsov <zummenix@gmail.com>
2020-03-28 13:30:23 +0500

    Add structopt and handle command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
 use serde::Deserialize;
 use std::error;
+use structopt::{clap::arg_enum, StructOpt};
+
+arg_enum! {
+    #[derive(PartialEq, Debug)]
+    enum Answer {
+        Yes,
+        No,
+        Maybe
+    }
+}
+
+#[derive(StructOpt, Debug)]
+/// Get funny gifs from https://yesno.wtf
+struct Opt {
+    #[structopt(possible_values = &Answer::variants())]
+    #[structopt(hide_possible_values = true, case_insensitive = true)]
+    /// Answer either `yes`, `no` or `maybe`
+    answer: Option<Answer>,
+}
 
 #[derive(Deserialize, Debug)]
 struct Resp {
-    answer: String,
-    forced: bool,
-    image: String
+    image: String,
+}
+
+fn url(answer: Option<Answer>) -> String {
+    let mut base_url = String::from("https://yesno.wtf/api");
+    match answer {
+        Some(Answer::Yes) => base_url.push_str("?force=yes"),
+        Some(Answer::No) => base_url.push_str("?force=no"),
+        Some(Answer::Maybe) => base_url.push_str("?force=maybe"),
+        None => (),
+    }
+    base_url
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let resp: Resp = minreq::get("https://yesno.wtf/api").send()?.json()?;
+    let opt = Opt::from_args();
+    let resp: Resp = minreq::get(url(opt.answer)).send()?.json()?;
     println!("{}", resp.image);
     Ok(())
 }
